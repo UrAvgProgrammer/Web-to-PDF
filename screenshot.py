6a3f8f15852bd5ae5847856885b3a7d7481a27f5
Jerald Talledo <jrldtlld@gmail.com>
2019-12-02 16:27:24 +0800

    added image to pdf function

--- screenshot.py
+++ screenshot.py
@@ -6,6 +6,7 @@ from fpdf import FPDF
 from PIL import Image
 import glob
 import os
+import uuid
 
 chrome_path = r"D:\Projects\chromedriver\chromedriver.exe"
 options = webdriver.ChromeOptions()
@@ -13,34 +14,41 @@ options.headless = True
 driver = webdriver.Chrome(chrome_path, chrome_options=options)
 driver.wait(15)
 
+
 def automation(url):
 	driver.get(url)
 	original_size = driver.get_window_size()
 	required_width = driver.execute_script('return document.body.parentNode.scrollWidth')
 	required_height = driver.execute_script('return document.body.parentNode.scrollHeight')
 	driver.set_window_size(required_width, required_height)
+	driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+	if not os.path.exists('img'):
+    	os.makedirs('img')
+
+    file_name = str(uuid.uuid4().hex)
+	driver.find_element_by_tag_name('body').screenshot('/img/{}.png'.format(file_name))
+	
+	driver.quit()
+
+def img_pdf(file_name):
+	if not os.path.exists('pdf'):
+    	os.makedirs('pdf')
+	# set here
+	image_directory = '/img/{}.png'.format(file_name)
+	 #add your image extentions
+	# set 0 if you want to fit pdf to image
+	# unit : pt
+	margin = 10
+
+    screenshot = Image.open(image_directory)
+    width, height = screenshot.size
+
+	pdf = FPDF(unit="pt", format=[width + 2*margin, height + 2*margin], 'A4')
+
+	pdf.add_page()
+	
+	pdf.image(image_directory, margin, margin)
 
 
-# def img_pdf():
-# 	# set here
-# 	image_directory = '/path/to/imageDir'
-# 	extensions = ('*.jpg','*.png','*.gif') #add your image extentions
-# 	# set 0 if you want to fit pdf to image
-# 	# unit : pt
-# 	margin = 10
-
-# 	imagelist=[]
-# 	for ext in extensions:
-# 	    imagelist.extend(glob.glob(os.path.join(image_directory,ext)))
-
-# 	for imagePath in imagelist:
-# 	    cover = Image.open(imagePath)
-# 	    width, height = cover.size
-
-# 	pdf = FPDF(unit="pt", format=[width + 2*margin, height + 2*margin])
-# 	pdf.add_page()
-
-# 	pdf.image(imagePath, margin, margin)
-
-# 	destination = os.path.splitext(imagePath)[0]
-# 	pdf.output(destination + ".pdf", "F")
+	pdf.output("/pdf/" + filename + ".pdf", "F")
